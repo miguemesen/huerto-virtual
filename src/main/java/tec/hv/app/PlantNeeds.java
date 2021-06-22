000abf93fd6825c21c4dee6b7bdf3c54f268c5dc
JosueMartinez <josuemartinez.cr@gmail.com>
2021-06-22 03:41:23 -0600

    details

--- src/main/java/tec/hv/app/PlantNeeds.java
+++
@@ -1,9 +0,0 @@
-package tec.hv.app;
-
-public interface PlantNeeds {
-	public abstract void regar(boolean condicion);
-	public abstract void abonar(boolean condicion);
-	public abstract void eliminarPlaga(boolean condicion);
-	public abstract void estado(boolean condicion);
-	public abstract void cosechar(boolean condicion);
-}
